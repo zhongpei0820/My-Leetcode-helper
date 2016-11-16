2468fc30c27e89f45611313739eeead02d714b1a
zhongpei0820 <kevinzhongpei@163.com>
2016-11-15 21:59:11 -0500

    crawl all problems info without casperJS

---
+++ importProblems.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+/***
+*      Import problems from leetcode, including id, title, link, difficulty and status.     
+*      And insert those imformation into the database.
+***/
+
+'use strict';
+const
+    cherrio = require('cheerio'),
+    request = require('request'),
+    insertProblems = require('./lib/db.js').insertProblems;
+
+let collection = 'problems',
+    url = 'https://leetcode.com/api/problems/algorithms/',
+    problems = [];
+
+request.get(url, (err,res,body) => {
+    if(!err && res.statusCode === 200){  
+        body = JSON.parse(body);
+        let allProblemInfo = body.stat_status_pairs;
+        for(let i in allProblemInfo){
+            let problem = {
+                "id" : allProblemInfo[i].stat.question_id,
+                "title" : allProblemInfo[i].stat.question__title,
+                "link" : allProblemInfo[i].stat.question__title_slug,
+                "difficulty" : allProblemInfo[i].difficulty.level,
+                "status" : allProblemInfo[i].status
+            }
+            problems.push(problem);
+        }
+        insertProblems(collection,problems);
+    }
+});
+
+
