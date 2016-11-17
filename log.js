b8925b51a8bcb7f4fe1a6a54615f4eaa93d888e5
zhongpei0820 <kevinzhongpei@163.com>
2016-11-16 23:17:05 -0500

    log problems you want to push

--- log.js
+++ log.js
@@ -1,31 +1,46 @@
+#!/usr/bin/env node
 'use strict';
-const fs = require('fs');
+const
+    fs = require('fs'),
+    queryProblem = require('./lib/db.js').queryProblem;
 let problem = process.argv[2],
     lang = process.argv[3];
 
-if(!problem || !/^\d$/.test(problem)) throw Error(`Invalid problem number "${problem}"`);
+if(!problem || !/^\d+$/.test(problem)) throw Error(`Invalid problem number "${problem}"`);
+
+problem = parseInt(problem);
+
 if(!lang) lang = 'java';
-fs.open('temp.json','r+',(err,fd) => {
-    if(err){
-        if(err.code === "ENOENT"){
-            writeProblems([{id : problem, lang : lang}]);
-            return;
-        }else{
-            throw err;
-        }
-    }
-    fs.readFile('temp.json',(err,data) => {
-        if(err) throw err;
-        data = JSON.parse(data);
-        data.push({id : problem, lang : lang});
-        writeProblems(data);
-    });
+
+queryProblem("problems", problem, (res)=> {
+    let data = {id : problem, title : res.title, lang : lang, link : res.link};
+    writeFile(data);
 });
+
+let writeFile = function(data){
+    fs.open('temp.json','r+',(err,fd) => {
+        if(err){
+            if(err.code === "ENOENT"){
+                writeProblems([data]);
+                return;
+            }else{
+                throw err;
+            }
+        }
+        fs.readFile('temp.json',(err,fd) => {
+            if(err) throw err;
+            fd = JSON.parse(fd);
+            fd.push(data);
+            writeProblems(fd);
+        });
+    });
+};
+
 
 let writeProblems = function(data){
      fs.writeFile('temp.json',JSON.stringify(data),err => {
         if(err) throw err;
-        console.log(`Saved!`);
+        console.log(`Logged!`);
     });
 };
 
